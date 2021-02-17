fd2238cd38b65ac20ece54cdaed02ed995516623
GSRHackZ <79179978+GSRHackZ@users.noreply.github.com>
2021-02-17 13:45:46 -0500

    Create RFE.js

---
+++ RFE.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Remove Flaticon Expiration
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Takes away time expiration from images.
+// @author       GSRHackZ
+// @match        https://www.flaticon.com/free-icon/*
+// @grant        none
+// @icon         https://www.flaticon.com/svg/1792/1792902.svg
+// @license                  MIT
+// @compatible               chrome
+// @compatible               firefox
+// @compatible               opera
+// @compatible               safari
+// ==/UserScript==
+
+let finished=false;
+
+document.addEventListener('keyup',function(evt){
+    if(evt.ctrlKey && evt.altKey && evt.keyCode==70){
+        if(!finished){
+            let ogSrc=document.getElementsByClassName("main-icon-without-slide pd-lv4 icon-png-container")[0].children[0];
+            if(ogSrc!==undefined){
+                let newSrc=ogSrc.src.split("svg/vstatic/")[0]+ogSrc.src.split("svg/vstatic/")[1].split("?")[0];
+                ogSrc.parentNode.style="transition:.6s;opacity:0%;";
+                ogSrc.src=newSrc;
+                setTimeout(function(){
+                    ogSrc.parentNode.style="transition:.6s;opacity:100%;";
+                    finished=true;
+                },1000)
+            }
+        }
+    }
+})
